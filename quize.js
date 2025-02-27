159cd674f501b83d30c3fd790a2b0fcdfaac14c0
JAYESH241524 <jd588410@gmail.com>
2025-02-27 12:07:22 +0530

    money level set

--- quize.js
+++ quize.js
@@ -1,5 +1,22 @@
 const welcomeScreen=document.getElementById("welcomeScreen");
 const quizeArea=document.getElementById("quizeArea");
+
+const levels=["₹10,00,000"
+    ,"₹5,00,000"
+    ,"₹2,50,000"
+    ,"₹1,25,000"
+    ,"₹64,000"
+    ,"₹32,000"
+    ,"₹16,000"
+    ,"₹8,000"
+    ,"₹4,000"
+    ,"₹2,000"
+    ,"₹1,000"
+    ,"₹500"
+    ,"₹300"
+    ,"₹200"
+    ,"₹100"
+];
  function startGame(){
     const userName=document.getElementById("userName").value;
     const error=document.getElementById("nameerror");
@@ -11,4 +28,21 @@ const quizeArea=document.getElementById("quizeArea");
     welcomeScreen.classList.add("hide");
     quizeArea.classList.remove("hide");
     
+    
  }
+ function setLevels(){
+    const levelList=document.getElementById("levelList");
+    levels.forEach((level,index)=>{
+       let listItem=document.createElement("li");
+       listItem.classList.add("levelListItems");
+       if(levels.length===index+1){
+        listItem.classList.add("activeLevel");
+       }
+       listItem.innerHTML=
+       `<span class="levelNumber">${levels.length-index}.</span>
+       <span class="levelAmount">${level}</span>`;
+       levelList.appendChild(listItem);
+
+    });
+ }
+ setLevels();
